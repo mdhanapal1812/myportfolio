74945b2fe83e898b31b6f7e0457ed06ded8dd3af
mdhanapal1812 <muthouazhagi1812@gmail.com>
2020-05-30 03:50:49 -0400

    Updates

---
+++ precache-manifest.4e675bbab46124c0dcb847de1d1b86d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2cec409dd9b77960418daa54fb5a3c",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "b0e1f64a00845cdbfc0d",
+    "url": "/myportfolio/static/js/2.199ec06c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.199ec06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107877dc6d822c3f70ad",
+    "url": "/myportfolio/static/js/main.a827884d.chunk.js"
+  },
+  {
+    "revision": "c186aebe7e03fd34214b",
+    "url": "/myportfolio/static/js/runtime-main.de89fd00.js"
+  }
+]);
